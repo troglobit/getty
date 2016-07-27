364f3c22d8c5097a1be8ae7a1281dc43e6a324df
Joachim Nilsson <troglobit@gmail.com>
2016-07-27 15:30:47 +0200

    Convert to Linux KNF
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- getty.c
+++ getty.c
@@ -44,157 +44,193 @@
 #include <sys/utsname.h>
 #include <stdio.h>
 
-char LOGIN[] =		"/usr/bin/login";
-char SHELL[] =		"/bin/sh";
+char LOGIN[] = "/usr/bin/login";
+char SHELL[] = "/bin/sh";
 
 /* Crude indication of a tty being physically secure: */
 #define securetty(dev)		((unsigned) ((dev) - 0x0400) < (unsigned) 8)
 
 void std_out(const char *s)
 {
-  write(1, s, strlen(s));
+	write(1, s, strlen(s));
 }
 
-/* Read one character from stdin.
+/*
+ * Read one character from stdin.
  */
 int readch(char *tty)
 {
-  int st;
-  char ch1;
+	int st;
+	char ch1;
 
-  /* read character from TTY */
-  st = read(0, &ch1, 1);
-  if (st == 0) {
-	std_out("\n");
-	exit(0);
-  }
-  if (st < 0) {
-	std_out("getty: ");
-	std_out(tty);
-	std_out(": read error\n");
-	exit(1);
-  }
-  return(ch1 & 0xFF);
+	/*
+	 * read character from TTY
+	 */
+	st = read(0, &ch1, 1);
+	if (st == 0) {
+		std_out("\n");
+		exit(0);
+	}
+
+	if (st < 0) {
+		std_out("getty: ");
+		std_out(tty);
+		std_out(": read error\n");
+		exit(1);
+	}
+
+	return ch1 & 0xFF;
 }
 
-
-/* Handle the process of a GETTY.
+/*
+ * Handle the process of a GETTY.
  */
 void do_getty(char *tty, char *name, size_t len, char **args)
 {
-  register char *np, *s, *s0;
-  int ch;
-  struct utsname utsname;
-  char **banner, *t;
-  static char *def_banner[] = { "%s  Release %r Version %v  (%t)\n\n%n login: ", 0 };
+	int ch;
+	char *np, *s, *s0;
+	char **banner, *t;
+	struct utsname utsname;
+	static char *def_banner[] = { "%s  Release %r Version %v  (%t)\n\n%n login: ", 0 };
 
-  /* Clean up tty name. */
-  if (!strcmp(tty, "/dev/"))
-	  tty += 5;
+	/*
+	 * Clean up tty name.
+	 */
+	if (!strcmp(tty, "/dev/"))
+		tty += 5;
 
-  if (args[0] != NULL) {
-	char cmd[5+6+1]; /* "stty " + "115200" + '\0' */
-	int speed;
-	speed = atoi(args[0]);
-	snprintf(cmd, sizeof(cmd), "stty %d\n", speed);
-	system(cmd);
-  }
+	if (args[0] != NULL) {
+		char cmd[5 + 6 + 1];	/* "stty " + "115200" + '\0' */
 
-  /* Display prompt. */
-  ch = ' ';
-  *name = '\0';
-  while (ch != '\n') {
-	/* Get data about this machine. */
-	uname(&utsname);
+		snprintf(cmd, sizeof(cmd), "stty %d\n", atoi(args[0]));
+		system(cmd);
+	}
 
-	/* Print the banner. */
-	for (banner = def_banner; *banner != NULL; banner++) {
-		std_out(banner == args ? "\n" : " ");
-		s0 = *banner;
-		for (s = *banner; *s != 0; s++) {
-			if (*s == '\\') {
-				write(1, s0, s-s0);
-				s0 = s+2;
-				switch (*++s) {
-				case 'n':  std_out("\n"); break;
-				case 's':  std_out(" "); break;
-				case 't':  std_out("\t"); break;
-				case 0:	   goto leave;
-				default:   s0 = s;
-				}
-			} else
-			if (*s == '%') {
-				write(1, s0, s-s0);
-				s0 = s+2;
-				switch (*++s) {
-				case 's':  std_out(utsname.sysname); break;
-				case 'n':  std_out(utsname.nodename); break;
-				case 'r':  std_out(utsname.release); break;
-				case 'v':  std_out(utsname.version); break;
-				case 'm':  std_out(utsname.machine); break;
-				case 't':  std_out(tty); break;
-				case 0:	   goto leave;
-				default:   s0 = s-1;
+	/*
+	 * Display prompt.
+	 */
+	ch = ' ';
+	*name = '\0';
+	while (ch != '\n') {
+		/*
+		 * Get data about this machine.
+		 */
+		uname(&utsname);
+
+		/*
+		 * Print the banner.
+		 */
+		for (banner = def_banner; *banner != NULL; banner++) {
+			std_out(banner == args ? "\n" : " ");
+			s0 = *banner;
+			for (s = *banner; *s != 0; s++) {
+				if (*s == '\\') {
+					write(1, s0, s - s0);
+					s0 = s + 2;
+					switch (*++s) {
+					case 'n':
+						std_out("\n");
+						break;
+					case 's':
+						std_out(" ");
+						break;
+					case 't':
+						std_out("\t");
+						break;
+					case 0:
+						goto leave;
+					default:
+						s0 = s;
+					}
+				} else if (*s == '%') {
+					write(1, s0, s - s0);
+					s0 = s + 2;
+					switch (*++s) {
+					case 's':
+						std_out(utsname.sysname);
+						break;
+					case 'n':
+						std_out(utsname.nodename);
+						break;
+					case 'r':
+						std_out(utsname.release);
+						break;
+					case 'v':
+						std_out(utsname.version);
+						break;
+					case 'm':
+						std_out(utsname.machine);
+						break;
+					case 't':
+						std_out(tty);
+						break;
+					case 0:
+						goto leave;
+					default:
+						s0 = s - 1;
+					}
 				}
 			}
+ leave:
+			write(1, s0, s - s0);
 		}
-	    leave:
-		write(1, s0, s-s0);
-	}
 
-	np = name;
-	while ((ch = readch(tty)) != '\n') {
-		if (np < name + len) *np++ = ch;
+		np = name;
+		while ((ch = readch(tty)) != '\n') {
+			if (np < name + len)
+				*np++ = ch;
+		}
+		*np = '\0';
+		if (*name == '\0')
+			ch = ' ';	/* blank line typed! */
 	}
-	*np = '\0';
-	if (*name == '\0') ch = ' ';	/* blank line typed! */
-  }
 }
-
 
 /* Execute the login(1) command with the current
  * username as its argument. It will reply to the
  * calling user by typing "Password: "...
  */
 void do_login(char *name)
-{ 
-  struct stat st;
+{
+	struct stat st;
 
-  execl(LOGIN, LOGIN, name, (char *) NULL);
-  /* Failed to exec login.  Impossible, but true.  Try a shell, but only if
-   * the terminal is more or less secure, because it will be a root shell.
-   */
-  std_out("getty: can't exec ");
-  std_out(LOGIN);
-  std_out("\n");
-  if (fstat(0, &st) == 0 && S_ISCHR(st.st_mode) && securetty(st.st_rdev)) {
-	execl(SHELL, SHELL, (char *) NULL);
-  }
+	execl(LOGIN, LOGIN, name, (char *)NULL);
+
+	/*
+	 * Failed to exec login.  Impossible, but true.  Try a shell,
+	 * but only if the terminal is more or less secure, because it
+	 * will be a root shell.
+	 */
+	std_out("getty: can't exec ");
+	std_out(LOGIN);
+	std_out("\n");
+	if (fstat(0, &st) == 0 && S_ISCHR(st.st_mode) && securetty(st.st_rdev))
+		execl(SHELL, SHELL, (char *)NULL);
 }
-
 
 int main(int argc, char **argv)
 {
-  char name[30], *tty;
-  struct sigaction sa;
+	char name[30], *tty;
+	struct sigaction sa;
 
-  /* Don't let QUIT dump core. */
-  sigemptyset(&sa.sa_mask);
-  sa.sa_flags = 0;
-  sa.sa_handler = exit;
-  sigaction(SIGQUIT, &sa, NULL);
+	/*
+	 * Don't let QUIT dump core.
+	 */
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sa.sa_handler = exit;
+	sigaction(SIGQUIT, &sa, NULL);
 
-	std_out("getty: tty name unknown\n");
-	pause();
-	return(1);
-  tty = ttyname(0);
-  if (tty == NULL) {
-  }
+	tty = ttyname(0);
+	if (tty == NULL) {
+		std_out("getty: tty name unknown\n");
+		pause();
+		return (1);
+	}
 
-  do_getty(tty, name, sizeof(name), argv+1);	/* handle getty() */
-  name[29] = '\0';		/* make sure the name fits! */
+	do_getty(tty, name, sizeof(name), argv + 1);
+	name[29] = '\0';
+	do_login(name);
 
-  do_login(name);		/* and call login(1) if OK */
-
-  return(1);			/* never executed */
+	return 1;
 }
